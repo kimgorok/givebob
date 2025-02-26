5cd29d893476523aeda28dadaf1b8098502ddab9
kimgorok <kjyse2@naver.com>
2025-02-26 17:59:37 +0900

    성능 개선

--- front/background.js
+++ front/background.js
@@ -1,9 +1,3 @@
-// 익스텐션이 설치되거나 업데이트 될 때
-chrome.runtime.onInstalled.addListener(() => {
-  // 기본적으로 익스텐션 활성화
-  chrome.action.enable();
-});
-
 // 가천대학교 도메인인지 확인하는 함수
 function isGachonDomain(url) {
   return url && url.match(/^https?:\/\/([^\/]+\.)?gachon\.ac\.kr/);
@@ -11,14 +5,14 @@ function isGachonDomain(url) {
 
 // URL이 변경될 때마다 확인
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // 탭이 로딩되는 동안 여러 번 호출될 수 있으므로 URL 변경 시에만 확인
-  if (changeInfo.url) {
-    // 변경된 URL이 가천대학교 도메인인지 확인
-    if (isGachonDomain(tab.url)) {
+  // 페이지 로딩이 완료된 경우에만 확인
+  if (changeInfo.status === "complete" && tab.url) {
+    // 가천대학교 도메인인지 확인
+    if (tab.url.includes("gachon.ac.kr")) {
       // 가천대학교 도메인이면 익스텐션 활성화
-      chrome.action.enable(tabId); // 팝업은 자동으로 열지 않음
+      chrome.action.enable(tabId);
     } else {
-      // 다른 사이트에서는 (아이콘)비활성화
+      // 다른 사이트에서는 비활성화
       chrome.action.disable(tabId);
     }
   }
@@ -26,9 +20,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // 새 탭이 활성화될 때마다 확인
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
-  // 현재 활성화된 탭의 정보 가져옴
   const tab = await chrome.tabs.get(activeInfo.tabId);
-  if (isGachonDomain(tab.url)) {
+  if (tab.url && tab.url.includes("gachon.ac.kr")) {
     // 가천대학교 사이트면 활성화
     chrome.action.enable(activeInfo.tabId);
   } else {
